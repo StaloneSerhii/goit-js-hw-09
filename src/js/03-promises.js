5ab74a0f12b383da9597ff438a3dc493c1293ac6
Serhii <sergio.fly4405@gmail.com>
2023-01-07 14:08:18 +0200

    3 s

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import Notiflix from 'notiflix';
+
+const formDelay = document.querySelector(".form")
+const buttonDelay = document.querySelector("button")
+console.log(buttonDelay);
+
+formDelay.addEventListener("input", formDates);
+buttonDelay.addEventListener("click", formDelays)
+let startCreatePromise = null;
+
+const formDate = {}
+
+function formDates(e) {
+  formDate[e.target.name] = e.target.value
 }
+
+
+function formDelays (e) {
+  e.preventDefault();
+for (let i = 0; i < formDate.amount; i++) {
+console.log(i);
+ 
+
+}
+
+
+
+}
+
+
+// function createPromise(position, delay) {
+ 
+//   const shouldResolve = Math.random() > 0.3;
+//  const p = new Promise(res, rej)
+
+//   if (shouldResolve) {
+//     res(position, delay.delay)
+ 
+
+//   } else {
+//     rej(position, delay.delay)
+
+//   } return createPromise
+  
+// }
+
+
